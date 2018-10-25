9bdd7e4bceb48168a144a641dc83e9a8a006729d
Leszek Milasz <leszekmilasz@gmail.com>
2018-10-25 18:52:55 +0200

    Initial commit

---
+++ src/main/java/Traffic/RoadsInitialization.java
@@ -0,0 +1,10 @@
+package Traffic;
+
+import javax.json.JsonObject;
+
+public class RoadsInitialization {
+    public void init(){
+        Object jsonReader = ;
+        JsonObject object = jsonReader.readObject();
+    }
+}
